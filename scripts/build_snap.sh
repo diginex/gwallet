97c7f385670101bdf49f39221deba7c338914843
Andres G. Aragoneses <knocte@gmail.com>
2019-02-03 16:59:09 +0800

    fix pushd

--- scripts/build_snap.sh
+++ scripts/build_snap.sh
@@ -2,7 +2,12 @@
 set -e
 
 which snapcraft >/dev/null || (echo "Please install snapcraft first" && exit 1)
-pushd snap
+
+# simplest answer from stackoverflow to get the dir of this script (not the current dir)
+# (see https://stackoverflow.com/a/337006 )
+DIR_OF_THIS_SCRIPT=`dirname $0`
+pushd $DIR_OF_THIS_SCRIPT/snap
+
 snapcraft clean build-and-install-console-frontend -s build
 snapcraft clean build-and-install-console-frontend -s pull
 snapcraft
